15d8d14409ea182a3ab23ba5ccba9ac566aca2b4
TaeGit <tgsong827@gmail.com>
2023-05-20 15:33:20 +0900

    update StakingPool to be upgradeable

--- contracts/TomaasStakingPool.sol
+++ contracts/TomaasStakingPool.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSL-1.0
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
@@ -14,7 +15,10 @@ interface ITokenForRewards is IERC20Upgradeable {
     function mint(address to, uint256 amount) external;
 }
 
-contract TomaasStakingPool is OwnableUpgradeable, ERC721HolderUpgradeable
+contract TomaasStakingPool is
+    Initializable,
+    ERC721HolderUpgradeable,
+    OwnableUpgradeable
 {
     ITokenForRewards public tokenForRewards;
     IERC721Upgradeable public liquidityProviderNft;
@@ -23,7 +27,14 @@ contract TomaasStakingPool is OwnableUpgradeable, ERC721HolderUpgradeable
     bool stakingInitialized;
     uint256 public stakingStartTime;
 
-    constructor(IERC721Upgradeable _liquidityProviderNft, ITokenForRewards _tokenForRewards) {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    // function initialize() initializer public {
+    function initialize(IERC721Upgradeable _liquidityProviderNft, ITokenForRewards _tokenForRewards) initializer public {
+        __Ownable_init();
         tokenForRewards = _tokenForRewards;
         liquidityProviderNft = _liquidityProviderNft;
     }
@@ -32,6 +43,10 @@ contract TomaasStakingPool is OwnableUpgradeable, ERC721HolderUpgradeable
         require(!stakingInitialized, "Staking is already initialized.");
         stakingStartTime = block.timestamp;
         stakingInitialized = true;
+    }
+
+    function setRewardsClaimable(bool _enabled) public onlyOwner {
+        rewardsClaimable = _enabled;
     }
 
 }
--- test/TomaasStakingPool.js
+++ test/TomaasStakingPool.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
 
 require('dotenv').config();
 
@@ -16,41 +16,53 @@ describe("TomaaS Staking Pool", () => {
     let erc20MockContract;
     let stakingPoolContract;
 
-    before("deploy associated contracts",async () => {
+    before("deploy associated contracts", async () => {
         const [deployerSigner, clientSigner] = await ethers.getSigners();
         deployerAddress = deployerSigner.address;
         clientAddress = clientSigner.address;
 
+        // ERC20 Contract
+        const erc20MockContractFactory = await ethers.getContractFactory("ERC20Mock");
+        erc20MockContract = await upgrades.deployProxy(erc20MockContractFactory, ["USD Coin", "USDC"], {
+            initializer: "initialize",
+        });
+        await erc20MockContract.deployed();
+        console.log(`Address of ERC20 Mock Contract: ${erc20MockContract.address}`);
+
+        // LPN Contract
         const lpnContractFactory = await ethers.getContractFactory("TomaasLPN");
-        lpnContract = await lpnContractFactory.deploy();
+        lpnContract = await upgrades.deployProxy(lpnContractFactory, [], {
+            initializer: "initialize",
+        });
         await lpnContract.deployed();
         console.log(`Address of LPN Contract: ${lpnContract.address}`);
-        
-        const rwnContractFactory = await ethers.getContractFactory("TomaasRWN");
-        rwnContract = await rwnContractFactory.deploy();
-        await rwnContract.deployed();
-        console.log(`Address of RWN Contract: ${rwnContract.address}`)
-
-        const erc20MockContractFactory = await ethers.getContractFactory("ERC20Mock");
-        erc20MockContract = await erc20MockContractFactory.deploy();
-        await erc20MockContract.deployed();
-        console.log(`Address of ERC20 Mock Contract: ${erc20MockContract.address}`)
 
     });
 
     it("deploy and initialize tomaas-staking-pool-contract", async () => {
         const stakingPoolContractFactory = await ethers.getContractFactory("TomaasStakingPool");
-        stakingPoolContract = await stakingPoolContractFactory.deploy(
-            lpnContract.address,
+        stakingPoolContract = await upgrades.deployProxy(stakingPoolContractFactory, [
+            lpnContract.address, 
             erc20MockContract.address
-        );
+        ], {
+            initializer: "initialize",
+        });
         await stakingPoolContract.deployed();
-
         console.log(`Address of Staking Pool Contract: ${stakingPoolContract.address}`);
 
         // initialize Staking and set TokensClaimable true
-        // await stakingPoolContract.initStaking();
-        // await stakingPoolContract.setTokenClaimable(true);
-        // console.log("StakingSystemContract is initialized");
-    })
+        await stakingPoolContract.initStaking();
+        await stakingPoolContract.setRewardsClaimable(true);
+        console.log("StakingSystemContract is initialized");
+    });
+
+    it("set Approval and Mint NFT", async () => {
+        // set Approval For All in the NFT Contract to the Staking Pool Contract
+        console.log((stakingPoolContract.address, clientAddress, 0));
+        await expect(
+            lpnContract.setApprovalForAll(stakingPoolContract.address, true)
+        )
+            .to.emit(lpnContract, "ApprovalForAll")
+            .withArgs(deployerAddress, stakingPoolContract.address, true);
+    });
 });
