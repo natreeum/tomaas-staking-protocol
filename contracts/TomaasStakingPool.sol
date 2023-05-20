448af981982af297a2316a5f16d9ef4d0b1b6007
TaeGit <tgsong827@gmail.com>
2023-05-20 16:47:30 +0900

    add stake function to StakingPool Contract

--- contracts/TomaasStakingPool.sol
+++ contracts/TomaasStakingPool.sol
@@ -23,14 +23,47 @@ contract TomaasStakingPool is
     ITokenForRewards public tokenForRewards;
     IERC721Upgradeable public liquidityProviderNft;
 
+    // [ Data of Staking Pool Contract ]
     bool public rewardsClaimable;
     bool stakingInitialized;
+
     uint256 public stakingStartTime;
+    uint256 public stakedTotal;
+
+    uint256 constant stakingTime = 180 seconds;
+    uint256 constant token = 10e18;
+
+    // [ Data of Staker ]
+    mapping(address => Staker) public stakerMap;
+
+    mapping(uint256 => address) public tokenOwnerMap;
+
+    struct Staker {
+        uint256[] tokenIds;
+        mapping(uint256 => uint256) tokenStakingCoolDown;
+        uint256 rewardsBalance;
+        uint256 rewardsReleased;
+    }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
+
+    /// @notice event emitted when a user has staked a nft
+    event Staked(address owner, uint256 amount);
+
+    /// @notice event emitted when a user has unstaked a nft
+    event Unstaked(address owner, uint256 amount);
+
+    /// @notice event emitted when a user claims reward
+    event RewardPaid(address indexed user, uint256 reward);
+
+    /// @notice Allows reward tokens to be claimed
+    event ClaimableStatusUpdated(bool status);
+
+    /// @notice Emergency unstake tokens without rewards
+    event EmergencyUnstake(address indexed user, uint256 tokenId);
 
     // function initialize() initializer public {
     function initialize(IERC721Upgradeable _liquidityProviderNft, ITokenForRewards _tokenForRewards) initializer public {
@@ -47,6 +80,53 @@ contract TomaasStakingPool is
 
     function setRewardsClaimable(bool _enabled) public onlyOwner {
         rewardsClaimable = _enabled;
+        emit ClaimableStatusUpdated(_enabled);
+    }
+
+    // Staking
+    function getStakedTokens(address _userAddres) public view returns (uint256[] memory tokenids) {
+        return stakerMap[_userAddres].tokenIds;
+    }
+
+    function stakeToken(uint256 tokenId) public {
+        require(
+            block.timestamp >= stakingStartTime,
+            "Staking Pool is not initialized yet"
+        );
+        _stake(msg.sender, tokenId);
+    }
+
+    function stakeTokens(uint256[] memory tokenIds) public {
+        require(
+            block.timestamp >= stakingStartTime,
+            "Staking Pool is not initialized yet"
+        );
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            _stake(msg.sender, tokenIds[i]);
+        }
+    }
+
+    function _stake(address _userAddress, uint256 _tokenId) internal {
+        require(
+            stakingInitialized,
+            "Staking Pool is not initialized yet"
+        );
+        require(
+            liquidityProviderNft.ownerOf(_tokenId) == _userAddress,
+            "The User is not the owner of this token"
+        );
+
+        Staker storage staker = stakerMap[_userAddress];
+        staker.tokenIds.push(_tokenId);
+        staker.tokenStakingCoolDown[_tokenId] = block.timestamp;
+        
+        tokenOwnerMap[_tokenId] = _userAddress;
+
+        liquidityProviderNft.approve(address(this), _tokenId);
+        liquidityProviderNft.safeTransferFrom(_userAddress, address(this), _tokenId);
+
+        emit Staked(_userAddress, _tokenId);
+        stakedTotal++;
     }
 
 }
